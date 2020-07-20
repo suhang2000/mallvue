01c3c6d5196f008ca1b673d93ec514757de97c3d
胡文哲 <1572065111@qq.com>
2020-07-20 18:56:42 +0800

    新增一个List的对于商品管理的测试页面

---
+++ src/components/List.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="list">
+  <body>
+    <h1 v-on:click="showGoodsList">显示商品列表</h1>
+  </body>
+  </div>
+</template>
+
+<script>
+    export default {
+      name: "List",
+      data() {
+        return {
+          //查询属性
+          queryInfo:{
+            query : '',
+            pagenum : 1,
+            pagesize :10
+          }
+        }
+      },
+      created(){
+        this.showGoodsList()
+      },
+      methods: {
+        //分页式展示商品信息
+        async showGoodsList() {
+          this.$axios
+            .get('/list/product')
+            .then(successResponse => {
+              console.log(successResponse.data)
+              if (successResponse.data.code === 400) {
+                alert('商品不存在')
+              }
+            })
+            .catch(failResponse => {
+              alert('服务器异常')
+            })
+
+        }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '../components/HelloWorld'
 import Login from '../components/Login'
-
+import List from '../components/List'
 Vue.use(Router)
 
 export default new Router({
@@ -16,6 +16,11 @@ export default new Router({
       path: '/login',
       name: 'Login',
       component: Login
+    },
+    {
+      path: '/list',
+      name: 'List',
+      component: List
     }
   ]
 })
