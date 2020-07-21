9ccf27500c232c1fa7016b9b5b08f07a7056dcde
李苏航 <572116254@qq.com>
2020-07-21 22:03:49 +0800

    改错 7/21/22点03分

--- src/store/index.js
+++ src/store/index.js
@@ -5,13 +5,13 @@ export default new Vuex.Store({
   state: {
     // 仿照user添加admin和saler的session存储信息，然后在main.js里改认证判断
     user: {
-      uname: window.SessionStorage.getItem('user' || '[]') == null ? '' : JSON.parse(window.SessionStorage.getItem('user' || '[]')).uname
+      uname: window.sessionStorage.getItem('user' || '[]') == null ? '' : JSON.parse(window.sessionStorage.getItem('user' || '[]')).uname
     }
   },
   mutations: {
     login (state, user) {
       state.user = user
-      window.SessionStorage.setItem('user', JSON.stringify(user))
+      window.sessionStorage.setItem('user', JSON.stringify(user))
     }
   }
 })
