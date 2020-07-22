fc225229b4157dc49333abaef89befa4b9b29a3c
胡万龙 <18146856052@163.com>
2020-07-22 18:00:32 +0800

    整合登录功能

---
+++ src/components/Login.vue
@@ -0,0 +1,136 @@
+<template>
+  <body id="poster">
+  <el-form :model="loginForm" :rules="rules" class="login-container" label-position="left"
+           label-width="0px">
+    <h3 class="login_title">登录</h3>
+    <el-form-item prop="name">
+      <el-input type="text" v-model="loginForm.name"
+                auto-complete="off" placeholder="账号"></el-input>
+    </el-form-item>
+    <el-form-item prop="password">
+      <el-input type="password" v-model="loginForm.password"
+                auto-complete="off" placeholder="密码" show-password></el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" style="width: 25%;background: #505458;border: none" v-on:click="user_login">用户</el-button>
+      <el-button type="primary" style="width: 25%;background: #505458;border: none" v-on:click="saler_login">商家</el-button>
+      <el-button type="primary" style="width: 25%;background: #505458;border: none" v-on:click="admin_login">管理</el-button>
+    </el-form-item>
+  </el-form>
+  </body>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data () {
+    return {
+      rules: {
+        name: [{required: true, message: '账号不能为空', trigger: 'blur'}],
+        password: [{required: true, message: '密码不能为空', trigger: 'blur'}]
+      },
+      loginForm: {
+        name: '',
+        password: ''
+      },
+      loading: false
+    }
+  },
+  methods: {
+    user_login () {
+      var _this = this
+      this.$axios
+        .post('/login/user', {
+          uname: this.loginForm.name,
+          password: this.loginForm.password
+        })
+        .then(resp => {
+          if (resp.data.code === 200) {
+            _this.$store.commit('loginUser', _this.loginForm)
+            const path = _this.$route.query.redirect
+            _this.$router.replace({path: path === '/' || path === undefined ? '/hello' : path})
+          } else {
+            this.$alert(resp.data.message, '提示', {
+              confirmButtonText: '确定'
+            })
+          }
+        })
+        .catch(failResponse => {
+          this.$message('服务器异常')
+        })
+    },
+    saler_login () {
+      var _this = this
+      this.$axios
+        .post('/login/saler', {
+          sname: this.loginForm.name,
+          password: this.loginForm.password
+        })
+        .then(resp => {
+          if (resp.data.code === 200) {
+            _this.$store.commit('loginSaler', _this.loginForm)
+            const path = _this.$route.query.redirect
+            _this.$router.replace({path: path === '/' || path === undefined ? '/hello' : path})
+          } else {
+            this.$alert(resp.data.message, '提示', {
+              confirmButtonText: '确定'
+            })
+          }
+        })
+        .catch(failResponse => {
+          this.$message('服务器异常')
+        })
+    },
+    admin_login () {
+      var _this = this
+      this.$axios
+        .post('/login/admin', {
+          aname: this.loginForm.name,
+          password: this.loginForm.password
+        })
+        .then(resp => {
+          if (resp.data.code === 200) {
+            _this.$store.commit('loginAdmin', _this.loginForm)
+            const path = _this.$route.query.redirect
+            _this.$router.replace({path: path === '/' || path === undefined ? '/admin/dashboard' : path})
+          } else {
+            this.$alert(resp.data.message, '提示', {
+              confirmButtonText: '确定'
+            })
+          }
+        })
+        .catch(failResponse => {
+          this.$message('服务器异常')
+        })
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .login-container {
+    border-radius: 15px;
+    background-clip: padding-box;
+    margin: 90px auto;
+    width: 350px;
+    padding: 35px 35px 15px 35px;
+    background: #fff;
+    border: 1px solid #eaeaea;
+    box-shadow: 0 0 25px #cac6c6;
+  }
+  .login_title {
+    margin: 0 auto 40px auto;
+    text-align: center;
+    color: #505458;
+  }
+  #poster {
+    background: url("../assets/admin/normalBG.jpeg") no-repeat center;
+    height: 100%;
+    width: 100%;
+    background-size: cover;
+    position: fixed;
+  }
+  body{
+    margin: 0;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,10 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '../components/HelloWorld'
 import List from '../components/saler/List'
-import Login from '../components/user/UserLogin'
-import AdminLogin from '../components/admin/AdminLogin'
+import Login from '../components/Login'
 import AdminRegister from '../components/admin/AdminRegister'
 import AdminIndex from '../components/admin/surface/AdminIndex'
 import AdminDashboard from '../components/admin/index'
-import SalerLogin from '../components/saler/SalerLogin'
 import SalerRegister from '../components/saler/SalerRegister'
 
 Vue.use(Router)
@@ -23,14 +21,9 @@ export default new Router({
       }
     },
     {
-      path: '/login/user',
+      path: '/login',
       name: 'Login',
       component: Login
-    },
-    {
-      path: '/login/saler',
-      name: 'SalerLogin',
-      component: SalerLogin
     },
     {
       path: '/register/saler',
@@ -41,11 +34,6 @@ export default new Router({
       path: '/list',
       name: 'List',
       component: List
-    },
-    {
-      path: '/login/admin',
-      name: 'AdminLogin',
-      component: AdminLogin
     },
     {
       path: '/register/admin',
@@ -72,61 +60,3 @@ export default new Router({
     }
   ]
 })
-
-// 用于创建默认路由
-// export const createRouter = routes => new Router({
-//   mode: 'history',
-//   routes: [
-//     {
-//       path: '/hello',
-//       name: 'HelloWorld',
-//       component: HelloWorld,
-//       meta: {
-//         requireAuth: true
-//       }
-//     },
-//     {
-//       path: '/login',
-//       name: 'Login',
-//       component: Login
-//     },
-//     {
-//       path: '/saler/login',
-//       name: 'SalerLogin',
-//       component: SalerLogin
-//     },
-//     {
-//       path: '/list',
-//       name: 'List',
-//       component: List
-//     },
-//     {
-//       path: '/admin/login',
-//       name: 'AdminLogin',
-//       component: AdminLogin
-//     },
-//     {
-//       path: '/admin/register',
-//       name: 'AdminRegister',
-//       component: AdminRegister
-//     },
-//     {
-//       path: '/admin',
-//       name: 'Admin',
-//       component: AdminIndex,
-//       meta: {
-//         requireAuth: true
-//       },
-//       children: [
-//         {
-//           path: '/admin/dashboard',
-//           name: 'Dashboard',
-//           component: AdminDashboard,
-//           meta: {
-//             requireAuth: true
-//           }
-//         }
-//       ]
-//     }
-//   ]
-// })
