7cc9552e8fa9cb39f9030239af382d5656f379d7
胡文哲 <1572065111@qq.com>
2020-07-23 14:14:55 +0800

    新增订单页面

--- src/components/saler/GoodsInfo.vue
+++ src/components/saler/GoodsInfo.vue
@@ -9,40 +9,38 @@
       border stripe>
 
       <el-table-column
-        label="商品名"
+        label="商品编号"
         width="100"
-        prop = "pname"
+        prop ="1"
       >
       </el-table-column>>
       <el-table-column
-        prop="price"
-        label="售出价格"
-        width="100">
+        label="商品名"
+        width="100"
+        prop ="1">
       </el-table-column>
       <el-table-column
-        prop="number"
-        label="售出数量"
-        width="100">
+        label="下单时间"
+        width="100"
+        prop="2">
       </el-table-column>
       <el-table-column
-      label="下单时间"
-      prop = "商品状态"
+      label="下单数量"
+      prop = "3"
     >
     </el-table-column>
       <el-table-column
       label="买家地址"
-      prop = "商品状态"
+      prop = "4"
     >
     </el-table-column>
       <el-table-column
-        prop="description"
-        label="买家备注"
-        show-overflow-tooltip>
+        label="支付状态"
+        prop="5">
       </el-table-column>
       <el-table-column
-        label="发货状态"
-        width="100"
-        prop = "商品状态"
+        label="发货"
+        prop = "6"
       >
       </el-table-column>
     </el-table>
@@ -57,7 +55,15 @@
       return {
         orders : [],
         order:{
-          uid:1,
+          pname:'苹果',
+          oid:0,
+          uid:0,
+          pid:0,
+          trade_time:2000-1-1,
+          trade_num:0,
+          address:'北京市',
+          pay_or_not:0,
+          deliver_or_not:0
         }
       }
     },
@@ -75,7 +81,9 @@
           .then(successResponse => {
             if (successResponse && successResponse.status === 200) {
               _this.orders = successResponse.data
-              console.log(_this.orders)
+              _this.order = successResponse.data
+              console.log(_this.order)
+              console.log(_this.order.pname)
             }
           })
           .catch(failResponse => {
